923dfc49d6027857bc4f86c091cc052e411e525d
Raiman <wijayakaleb@gmail.com>
2019-09-17 16:26:21 +0700

    Initial Commit

---
+++ Steven TAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Steven TAI
+//
+//  Created by Kaleb Wijaya on 17/09/19.
+//  Copyright © 2019 Kaleb Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
