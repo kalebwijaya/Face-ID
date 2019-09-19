bbe70096cff6ab32c8db4efb6ab7e49b59329b7d
Raiman <wijayakaleb@gmail.com>
2019-09-19 22:14:10 +0700

    1.0

--- Steven TAI/ViewController.swift
+++ Steven TAI/ViewController.swift
@@ -7,13 +7,56 @@
 //
 
 import UIKit
+import LocalAuthentication
+
+enum AuthenticationState {
+    case loggedin, loggedout
+}
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var label: UILabel!
+    var context = LAContext()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        context.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil)
+        
+        state = .loggedout
     }
+    
+    var state = AuthenticationState.loggedout {
+        didSet {
+            btnLogin.isHighlighted = state == .loggedin
+            self.view.backgroundColor = state == .loggedin ? .green : .red
+            label.isHidden = (state == .loggedin) || (context.biometryType != .faceID)
+        }
+    }
+    
+    @IBAction func btnTap(_ sender: Any) {
+        if state == .loggedin {
+            state = .loggedout
+        } else {
+            context = LAContext()
+            context.localizedCancelTitle = "Enter Username/Password"
+            var error: NSError?
+            if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+                let reason = "Log in to your account"
+                context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason ) { success, error in
+                    if success {
+                        DispatchQueue.main.async { [unowned self] in
+                            self.state = .loggedin
+                            }
+                    } else {
+                        print(error?.localizedDescription ?? "Failed to authenticate")
+                        }
+                    }
+            }
+        }
+
+    }
+    
 
 
 }
